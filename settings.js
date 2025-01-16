560aeab5d40ca38a9e37a27daecc2ef9106413ac
botm0971 <botm0971@gmail.com>
2025-01-16 22:28:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "TmpxGKYD#0qzM2YCK21V17NK37ABmyYXJsi0urxSLjdDwbQl8ias", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 STATUES_REPLY: process.env.STATUES_REPLY || "true",
